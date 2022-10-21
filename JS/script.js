c246c3967542359139b666f967efa3e411fa5671
MarmoW <115847012+MarmoW@users.noreply.github.com>
2022-10-21 20:38:44 -0300

    Mais organizado e com timer
    ainda falta o flip e ajustes finais no layout mobile

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,16 @@
-let numcartas, mesa, mostrar, esconder,cobrejogo,reiniciar;
+let numcartas, mesa, mostrar, esconder,cobrejogo,reiniciar, timer1, codtimer;
 let deck = [];
-let test = [];
 let random = [];
 let recente1 = null;
 let recente2 = null;
 let jogadas = 0;
+let segundos = 0;
 cobrejogo = document.querySelector('.cobrejogo')
-test = document.querySelectorAll('.cartas');
 mesa = document.querySelector('.jogo');
-//<li class="cartas"><img src="Imgs/back.png"/></li>
-//cobrejogo.classList.add('on')
+
 
 comecar();
+
 function comecar() {
 numcartas = prompt("Escolha o número de cartas desejado, o número deve ser entre 4 e 14")
 if(numcartas >= 4 && numcartas <= 14){
@@ -21,6 +20,7 @@ if(numcartas >= 4 && numcartas <= 14){
     }
         else {
            // alert("O jogo vai começar");
+            codtimer = setInterval(timer2, 1000); //timer começa
             darcartas();
          } 
         
@@ -31,7 +31,7 @@ if(numcartas >= 4 && numcartas <= 14){
     }
 }
 
-function darcartas() {
+function darcartas() { //coloca as cartas na mesa
     for(let i = 0; i < numcartas; i++ ){
     random[i] = Math.floor(i/2);    
     }
@@ -56,28 +56,30 @@ function revelar1(parent) {
         revelar(parent);
     }
 }
-function flip(){
+function flip(){ // revela a carta *redundante por enquanto
     mostrar.classList.add('display');
     esconder.classList.add('sumiu');
 
 }
-function revelar(parent) {
+function revelar(parent) { // revela carta e se não tiver nada nas cartas anteriores adiciona antes de guardar a carta clicada agora
     if(recente1 === null){
         recente1 = mostrar;
         recente2 = esconder;  }  
 
-    mostrar = parent.querySelector(".parrot");
+    mostrar = parent.querySelector(".parrot"); 
     esconder = parent.querySelector(".back");
-    mostrar.classList.add('display');
-    esconder.classList.add('sumiu');
+    flip();
     
     
     if(mostrar.classList.value === recente1.classList.value){
-        recente1 = null;
-        recente2 = null;
+        recente1 = null; //limpa a carta guardada antes caso acerte
+        recente2 = null; 
         cobrejogo.classList.remove('on');
-        if(document.querySelectorAll('.display').length == numcartas){
-            alert(`Você venceu o jogo em ${jogadas} jogadas!`);
+        
+        if(document.querySelectorAll('.display').length == numcartas){ //encerra o jogo quando revelar todas cartas
+            
+            alert(`Você venceu o jogo em ${jogadas} jogadas e seu tempo foi ${Math.floor(segundos/60)}:${segundos%60}`);
+            clearInterval(codtimer); //timer para
             recomecar();
             
         }
@@ -90,7 +92,9 @@ function revelar(parent) {
 function recomecar() {
     reiniciar = prompt("Gostaria de jogar outra partida?")
     if(reiniciar === 'sim'){
-        mesa.innerHTML = "";
+        mesa.innerHTML = "";// limpa a mesa
+        random = [];        // limpa o array random
+        segundos = 0;       // zera o timer
         comecar();
     }
     else if(reiniciar ==='não'){
@@ -99,7 +103,7 @@ function recomecar() {
         recomecar();
     }
 }
-function hide() { 
+function hide() { //esconde a carta
 mostrar.classList.remove('display');
 esconder.classList.remove('sumiu');
 recente1.classList.remove('display');
@@ -114,3 +118,9 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
    
+function timer2() { //função do timer
+    segundos++;
+    timer1 = document.querySelector(".timer");
+    timer1.innerHTML = `${Math.floor(segundos/60)}:${segundos%60}`;
+      
+}
